c21e7b4c502e5d4e06fbef718dfb988fdf072867
Grace <mgracebodur@gmail.com>
2019-11-09 13:38:15 -0500

    Added Accordion component and Accordion test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import React, { Component } from "react";
 import "./App.css";
-import Messages from "./Messages";
-import Tabs from "./state/Tabs.js";
+import Accordion from './state-drills/Accordion'
+// import Messages from "./Messages";
+// import Tabs from "./state/Tabs.js";
 
-const tabsProp = [
+// const tabsProp = [
+//   {
+//     name: "First tab",
+//     content:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque."
+//   },
+//   {
+//     name: "Second tab",
+//     content:
+//       "Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit."
+//   },
+//   {
+//     name: "Third tab",
+//     content:
+//       "Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur."
+//   }
+// ];
+
+const sections = [
   {
-    name: "First tab",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque."
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
-    name: "Second tab",
-    content:
-      "Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
   },
   {
-    name: "Third tab",
-    content:
-      "Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur."
-  }
-];
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Tabs tabs={tabsProp} />
+        <Accordion sections={sections} />
       </div>
     );
   }
 }
 
 export default App;
-console.log('app')
+
---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+class Accordion extends React.Component {
+    static defaultProps = {
+        sections: [],
+    }
+
+    state = {
+        currentIndex: null
+    }
+
+    handleButtonClick(index) {
+        this.setState({currentIndex: index})
+    }
+
+    renderSections() {
+        return (
+            this.props.sections.map((section, index) => {
+                return (
+                    <div>
+                    <li key={index}>
+                        <button onClick={() => this.handleButtonClick(index)}>
+                            {section.title}
+                        </button>
+                        {this.renderContent(section,index)}
+                    </li>
+                    </div>
+                )
+            })
+        )
+    }
+
+    renderContent(section, index) {
+            if(this.state.currentIndex === index) {
+                return(
+                <p>{section.content}</p>
+                )
+            }
+    }
+
+    render () {
+        return(
+            <div>
+                <ul>
+                   {this.renderSections()}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Accordion
