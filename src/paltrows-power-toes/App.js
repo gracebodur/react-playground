3269826f0e190fecfa235c85fc60c4142ddf323e
Grace <mgracebodur@gmail.com>
2019-11-18 22:21:09 -0500

    Added Switch Component

--- src/paltrows-power-toes/App.js
+++ src/paltrows-power-toes/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import {Route} from 'react-router-dom'
+// import {Route} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Nav from './Nav'
 import Header from './Header'
 import Footer from './Footer'
 import HomePage from './HomePage'
 import AboutPage from './AboutPage'
+import WorkoutsPage from './WorkoutsPage'
+import NotFoundPage from './NotFoundPage'
 import './App.css'
 
 export default class App extends Component {
@@ -20,8 +23,12 @@ export default class App extends Component {
         <main>
           {/* <HomePage /> */}
           {/* <Route path='/' component ={HomePage} /> */}
+          <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/about' component={AboutPage} />
+          <Route path='/workouts' component={WorkoutsPage} />
+          <Route component={NotFoundPage} />
+          </Switch>
         </main>
         <footer>
           <Footer />
