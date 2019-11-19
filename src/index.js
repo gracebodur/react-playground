6b617d582dfd4e8c2986b2bbcad814e72123f933
Grace <mgracebodur@gmail.com>
2019-11-18 20:37:35 -0500

    Installed react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
+import App from "./App";
 import "./index.css";
-import AppLang from "./lang-context/AppLang";
+// import AppLang from "./lang-context/AppLang";
+import {BrowserRouter} from 'react-router-dom'
 
 // import Bomb from "./state-drills/RouletteGun";
 // import RouletteGun from "./state-drills/RouletteGun";
 
-ReactDOM.render(<AppLang />, document.getElementById("root"));
+ReactDOM.render(
+<BrowserRouter>
+<App/>
+</BrowserRouter>,
+ document.getElementById("root"));
