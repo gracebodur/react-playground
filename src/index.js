60cfc6c9da128b89b444879418a8e058909c2bb2
Grace <mgracebodur@gmail.com>
2019-11-27 01:54:53 -0500

    RegistrationForm practice done started ErrorB

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// 
-// import App from './paltrows-power-toes/App'
 import "./index.css";
-import AppLang from "./lang-context/AppLang";
+import App from './playingwithdanger/App'
+// import RegistrationForm from './registrationForm/registrationForm'
+// import AppLang from "./lang-context/AppLang";
+// import App from './paltrows-power-toes/App'
 // import {BrowserRouter} from 'react-router-dom'
-
 // import Bomb from "./state-drills/RouletteGun";
 // import RouletteGun from "./state-drills/RouletteGun";
 
-ReactDOM.render( <AppLang />, document.getElementById("root"));
+ReactDOM.render( <App />, document.getElementById("root"));
 
  //{/* <BrowserRouter>
 //<App/>
