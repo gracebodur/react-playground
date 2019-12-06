e7e64bdf78dd5a7a8e711897bea8d1ebe9170db2
Grace <mgracebodur@gmail.com>
2019-12-06 16:04:43 -0500

    pacticing

--- src/router-challenge/app.js
+++ src/router-challenge/app.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import AboutMenu from './about-menu';
 //5. import Switch and Route from react-router-dom
-import {Switch, Route} from 'react-router-dom'
+import { Route} from 'react-router-dom'
 
 
 export default function App(props) {
+
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
             {/* <AboutMenu /> */}
-            
-            <Switch>
-                <Route exact path = '/about/foo/bar'><AboutMenu /></Route>
-                <Route exact path = '/about/foo/'><AboutMenu /></Route>
-                <Route exact path = '/about/bar'><AboutMenu /></Route>
-            </Switch>
+            <Route path = '/about/:meta'><AboutMenu /></Route>
+            {/* <Switch>
+            <Route path = '/about/foo/bar'><AboutMenu /></Route> */}
+                {/* <Route exact path = '/about/foo/'><AboutMenu /></Route> */}
+                {/* <Route exact path = '/about/bar'><AboutMenu /></Route> */}
+            {/* </Switch>  */}
         </div>
     );
 }
