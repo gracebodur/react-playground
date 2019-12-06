82719b436b535b12f20b25e98fea8ee193a15c13
Grace <mgracebodur@gmail.com>
2019-12-06 02:17:50 -0500

    practicing

--- src/router-challenge/about-menu.js
+++ src/router-challenge/about-menu.js
@@ -1,9 +1,5 @@
 import React from 'react';
-//3. import NavLink from react-router-dom
-// import {NavLink} from 'react-router-dom'
 
-//4. wrap all list items to Link to with each seperate paths
-//5. remember to delete ul
 export default function AboutMenu(props) {
     return (
         <nav className="about-menu">
--- src/router-challenge/app.js
+++ src/router-challenge/app.js
@@ -4,19 +4,16 @@ import AboutMenu from './about-menu';
 import {Switch, Route} from 'react-router-dom'
 
 
-//6. wrap make path dynamic using the : colon, 
-//then wrap <AppMenu /> component in Route
 export default function App(props) {
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
             {/* <AboutMenu /> */}
-            {/* <Route path='/about/:foo'><AboutMenu /></Route>
-            <Route path='/about/:bar'><AboutMenu /></Route> */}
+            
             <Switch>
-                <Route exact path='/about/foo/bar'><AboutMenu /></Route>
-                <Route exact path='/about/foo'><AboutMenu /></Route>
-                <Route exact path='/about/bar'><AboutMenu /></Route>
+                <Route exact path = '/about/foo/bar'><AboutMenu /></Route>
+                <Route exact path = '/about/foo/'><AboutMenu /></Route>
+                <Route exact path = '/about/bar'><AboutMenu /></Route>
             </Switch>
         </div>
     );
